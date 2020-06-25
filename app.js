9770da5b4c964d5c6a4ef2eb30d8ec864259cb7c
utkarshgupta2504 <utkarshgupta2504@gmail.com>
2020-06-25 16:53:05 +0530

    Level 5:  Using the passport module to maintain sessions and increase security.

--- app.js
+++ app.js
@@ -3,15 +3,25 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const ejs = require('ejs');
-const bcrypt = require('bcrypt');
-
-const saltRounds = 10;
+var session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');	//For passport module.
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
+
+app.use(session({	//Maintains our session 
+
+	secret: "Our little secret.",
+	resave: false,
+	saveUninitialized: false
+}));
+
+app.use(passport.initialize());	//Initializes the passport module
+app.use(passport.session());	//Starts the passport session and instructs to use cookies.
 
 mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true, useUnifiedTopology: true});
 
@@ -21,11 +31,33 @@ const userSchema = new mongoose.Schema({
 	password: String
 });
 
+userSchema.plugin(passportLocalMongoose);	//Using the passport Plugin
+
 const User = mongoose.model('User', userSchema);
+
+passport.use(User.createStrategy());	//Creating strategy.
+
+passport.serializeUser(User.serializeUser());		//For serialising (Adding cookie).
+passport.deserializeUser(User.deserializeUser());	//For deserialising (Removing Cookie).
 
 app.get('/', (req, res) => {
 
 	res.render('home');
+})
+
+app.route('/secrets')
+
+.get((req, res) => {
+
+	if(req.isAuthenticated()) {		//Checks the cookie if present. If yes, authenicates.
+
+		res.render('secrets');
+	}
+
+	else {
+
+		res.redirect('/login');
+	}
 })
 
 app.route('/register')
@@ -38,28 +70,22 @@ app.route('/register')
 
 .post((req, res) => {
 
-		bcrypt.hash(req.body.password, saltRounds, (err, hash) => {	//Recieving the hash after 10 rounds of salting.
+		User.register({username: req.body.username}, req.body.password, (err, user) => {
 
-			const newUser = new User({
+			if(err) {
 
-				username: req.body.username,
-				password: hash
-			});
+				console.log(err);
+				res.redirect('/register');
+			}
 
-			newUser.save(err => {
+			else {
 
-				if(err) {
+				passport.authenticate('local')(req, res, () => {	//Authenticates the user and add cookie.
 
-					console.log(err);
-				}
-
-				else {
-
-					res.render('secrets');
-				}
-			})
-		});
-
+					res.redirect('/secrets');
+				})
+			}
+		})
 		
 	});
 
@@ -76,7 +102,13 @@ app.route('/login')
 	const username = req.body.username;
 	const password = req.body.password;
 
-	User.findOne({username: username}, (err, found) => {
+	const user = new User({
+
+		username: username,
+		password: password
+	});
+
+	req.login(user, err => {
 
 		if(err) {
 
@@ -85,28 +117,21 @@ app.route('/login')
 
 		else {
 
-			if(found) {
+			passport.authenticate('local')(req, res, () => {
 
-				bcrypt.compare(password, found.password, (err, res1) => {
-
-					if(res1) {
-
-						res.render('secrets');
-					}
-
-					else {
-
-						res.send('Incorrect Password, Please Try Again.')
-					}
-				})
-			}
-
-			else {
-
-				res.send('Username not registered. You may want to register yourself up.')
-			}
+				res.redirect('/secrets');
+			})
 		}
 	})
+})
+
+app.route('logout')
+
+.get((req, res) => {
+
+	req.logout();	//Destroys the cookie.
+
+	res.redirect('/');
 })
 
 app.listen(3000, () => {
